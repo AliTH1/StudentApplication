492fde756c40ff8d96018ffcc16fbcff93a8cbb1
Ali <alitaghizada2@gmail.com>
2023-01-04 13:59:22 +0400

    first commit

---
+++ StudentApplication/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Student student1 = new Student(202, "Rauf", "Mirəliyev", 50, 150, "Hazırlıq məktəbi - 403");
+
+            Console.WriteLine("Şağirdlə bağlı hansı əməliyyatı yerinə yetirmək istəyirsiz?");
+            Console.WriteLine("1 - Şağird haqqında məlumat" + "\n2 - Şağirdin ortalama qiyməti"
+                + "\n3 - Şağirdin oxuduqu məktəb" + "\n4 - Əməliyyatı dayandır");
+
+            Console.Write("\nSeçim: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (userInput)
+            {
+                case 1: student1.showStudentInformation(); break;
+                case 2: student1.averageStudent(); break;
+                case 3: student1.getSchoolName(); break;
+                case 4: Console.Clear(); break;
+                default: Console.WriteLine( "Girişin düzgünlüyünü yoxlayın"); break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StudentApplication/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApplication
+{
+    public class Student
+    {
+        private int studentNo;
+        private string studentName, studentSurname;
+        private int exam1, exam2, final;
+        private string schoolName;
+
+        public Student(int _studentNo, string _studentName, string _studentSurname,
+            int _exam1, int _exam2, string _schoolName)
+        {
+            studentNo = _studentNo;
+            studentName = _studentName;
+            studentSurname = _studentSurname;
+            exam1 = _exam1;
+            exam2 = _exam2;
+            final = _exam1 + _exam2;
+            schoolName = _schoolName;
+        }
+
+        public void showStudentInformation()
+        {
+            Console.Clear();
+            Console.WriteLine("\nŞagirdin nömrəsi: " + studentNo);
+            Console.WriteLine("Adı: " + studentName + " " + "\nSoyadı: " + studentSurname);
+            Console.WriteLine("\n\nBirinci imtahan balı: " + exam1 + " " + "\nİkinci imtahan balı: " + exam2
+                + "\nÜmumi bal: " + final);
+        }
+
+        public void averageStudent()
+        {
+            Console.Clear();
+            double ortalama = exam1 * 0.2 + exam2 * 0.2 + final * 0.6;
+            Console.WriteLine("Ortalama qiymət: " + ortalama);
+        }
+
+        public void getSchoolName()
+        {
+            Console.Clear();
+            Console.WriteLine("Məktəbin adı: " + schoolName);
+        }
+    }
+}
